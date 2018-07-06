c6545b3ebb8810e5f3781752e274abc90cafb1f0
Luis Hankel <kaes3kuch3n@gmail.com>
2018-07-06 02:07:08 +0200

    Initial commit

---
+++ src/Hashtable.java
@@ -0,0 +1,97 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Hashtable {
+
+    private List<String>[] hashtable;
+
+    public Hashtable() {
+        hashtable = new List[5003];
+    }
+
+    public void readFile(String file) {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+
+            String word;
+
+            while ((word = reader.readLine()) != null) {
+                if (word.length() != 7) {
+                    continue;
+                }
+
+                int index = calculateHashValue(word);
+
+                if (hashtable[index] == null) {
+                    hashtable[index] = new ArrayList<>();
+                }
+                hashtable[index].add(word);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private int calculateHashValue(String word) {
+        long hashvalue = 0;
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+
+        for (int i = 0; i < chars.length; i++) {
+            int characterValue = (int) chars[i] - 96;
+            hashvalue += Math.pow(characterValue, i);
+        }
+
+        return (int) (hashvalue % 5000);
+    }
+
+    public List<String>[] getHashtable() {
+        return hashtable;
+    }
+
+    public void printHastable() {
+        for (int i = 0; i < getHashtable().length; i++) {
+            List<String> list = getHashtable()[i];
+
+            StringBuilder sb = new StringBuilder();
+
+            if (list == null) continue;
+
+            for (String word : list) {
+                sb.append(word).append(", ");
+            }
+            System.out.println(i + ": " + sb);
+        }
+    }
+
+    public void checkWord(String word) {
+        List<String> list = getHashtable()[calculateHashValue(word)];
+
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+
+        for (String listEntry : list) {
+            char[] listEntryChars = listEntry.toCharArray();
+            Arrays.sort(listEntryChars);
+
+            if (Arrays.equals(chars, listEntryChars)) {
+                System.out.println(listEntry);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Hashtable hashtable = new Hashtable();
+        hashtable.readFile("words.txt");
+
+        hashtable.printHastable();
+
+        hashtable.checkWord("aaghhrr");
+    }
+
+}
